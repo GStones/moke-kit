fda98d0231f05197ccf2d4ff5ef4123cbaa32004
guolei <gl308@qq.com>
2023-09-07 17:00:56 +0800

    fix(tracing): remove tracing

--- server/internal/srpc/grpc_middleware.go
+++ server/internal/srpc/grpc_middleware.go
@@ -93,7 +93,6 @@ func addInterceptorOptions(
 	//authClient cli.AuthClient,
 	opts ...grpc.ServerOption,
 ) []grpc.ServerOption {
-	// Setup metrics.
 	srvMetrics := grpcprom.NewServerMetrics(
 		grpcprom.WithServerHandlingTimeHistogram(
 			grpcprom.WithHistogramBuckets([]float64{0.001, 0.01, 0.1, 0.3, 0.6, 1, 3, 6, 9, 20, 30, 60, 90, 120}),
