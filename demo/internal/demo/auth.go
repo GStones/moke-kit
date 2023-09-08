bcab4463eeee25394e9dcbdcd07a632396826faa
guolei <gl308@qq.com>
2023-09-08 11:53:55 +0800

    feat(auth):add auth function

--- demo/internal/demo/auth.go
+++ demo/internal/demo/auth.go
@@ -10,21 +10,24 @@ import (
 	"github.com/gstones/moke-kit/server/pkg/sfx"
 )
 
-type DemoAuth struct {
+// Author is a demo auth service
+type Author struct {
 }
 
-func (d *DemoAuth) Auth(token string) (string, error) {
+// Auth is a demo auth method
+func (d *Author) Auth(token string) (string, error) {
 	if !strings.Contains(token, "test") {
 		return "", errors.New("token error")
 	}
 	return token, nil
 }
 
+// AuthModule is a demo auth module
 var AuthModule = fx.Provide(
 	func(
 		l *zap.Logger,
 	) (out sfx.AuthServiceResult, err error) {
-		out.AuthService = &DemoAuth{}
+		out.AuthService = &Author{}
 		return
 	},
 )
