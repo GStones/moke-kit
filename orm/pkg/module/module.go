98c59af937658e1411a0e1974812f605dd72cafa
GuoLei <>
2023-09-28 18:15:41 +0800

    fix(orm): add  orm comment

--- orm/pkg/module/module.go
+++ orm/pkg/module/module.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gstones/moke-kit/orm/pkg/nfx"
 )
 
+// Module is the orm module
+// inject redis,mongo,gorm
 var Module = fx.Module("orm",
 	nfx.SettingsModule,
 	nfx.MongoPureModule,
