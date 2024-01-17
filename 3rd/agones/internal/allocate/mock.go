159179a76748d585cb6f88be6f25972768a90865
guolei <gl308@qq.com>
2024-01-17 11:49:02 +0800

    fix(agones): fix agones setting

--- 3rd/agones/internal/allocate/mock.go
+++ 3rd/agones/internal/allocate/mock.go
@@ -2,12 +2,16 @@ package allocate
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+	"strings"
 
 	allocation "agones.dev/agones/pkg/allocation/go"
 	"google.golang.org/grpc"
 )
 
 type MockAllocationServiceClient struct {
+	URL string
 }
 
 // Allocate is a mock implementation of Allocate
@@ -16,5 +20,24 @@ func (m *MockAllocationServiceClient) Allocate(
 	_ *allocation.AllocationRequest,
 	_ ...grpc.CallOption,
 ) (*allocation.AllocationResponse, error) {
-	return nil, nil
+	if m.URL == "" {
+		return nil, fmt.Errorf("mock allocation service client url is empty")
+	}
+	hosts := strings.Split(m.URL, ":")
+	if len(hosts) != 2 {
+		return nil, fmt.Errorf("mock allocation service client url:%v is invalid", m.URL)
+	}
+	port, err := strconv.Atoi(hosts[1])
+	if err != nil {
+		return nil, fmt.Errorf("mock allocation service client url:%v is invalid", m.URL)
+	}
+	res := &allocation.AllocationResponse{
+		Address: hosts[0],
+		Ports: []*allocation.AllocationResponse_GameServerStatusPort{
+			{
+				Port: int32(port),
+			},
+		},
+	}
+	return res, nil
 }
