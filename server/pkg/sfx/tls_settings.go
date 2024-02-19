4fae46b99238af29428fe1ffd9281336a01522b7
GStones <gl308123456@gmail.com>
2024-02-19 15:31:32 +0800

    fix(tls): fix tls setting (#111)

--- server/pkg/sfx/tls_settings.go
+++ server/pkg/sfx/tls_settings.go
@@ -29,14 +29,14 @@ type SecuritySettingsResult struct {
 	fx.Out
 
 	//client mTLS settings
-	ClientCaCert string `name:"ClientCaCert" envconfig:"CLIENT_CA_CERT" default:"./configs/x509/client_ca_cert.pem"`
-	ClientCert   string `name:"ClientCert" envconfig:"CLIENT_CERT" default:"./configs/x509/client_cert.pem"`
-	ClientKey    string `name:"ClientKey" envconfig:"CLIENT_KEY" default:"./configs/x509/client_key.pem"`
+	ClientCaCert string `name:"ClientCaCert" envconfig:"CLIENT_CA_CERT" default:"./configs/tls-client/ca.crt"`
+	ClientCert   string `name:"ClientCert" envconfig:"CLIENT_CERT" default:"./configs/tls-client/tls.crt"`
+	ClientKey    string `name:"ClientKey" envconfig:"CLIENT_KEY" default:"./configs/tls-client/tls.key"`
 
 	//server mTLS settings
-	ServerCACert string `name:"ServerCaCert" envconfig:"SERVER_CA_CERT" default:"./configs/x509/ca_cert.pem"`
-	ServerCert   string `name:"ServerCert" envconfig:"SERVER_CERT" default:"./configs/x509/server_cert.pem"`
-	ServerKey    string `name:"ServerKey" envconfig:"SERVER_KEY" default:"./configs/x509/server_key.pem"`
+	ServerCACert string `name:"ServerCaCert" envconfig:"SERVER_CA_CERT" default:"./configs/tls-server/ca.crt"`
+	ServerCert   string `name:"ServerCert" envconfig:"SERVER_CERT" default:"./configs/tls-server/tls.crt"`
+	ServerKey    string `name:"ServerKey" envconfig:"SERVER_KEY" default:"./configs/tls-server/tls.key"`
 	ServerName   string `name:"ServerName" envconfig:"SERVER_NAME" default:""`
 
 	TLSEnable bool `name:"TLSEnable" envconfig:"TLS_ENABLE" default:"false" `
