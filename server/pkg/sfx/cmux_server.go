d86af396a52251d4333055b439ac979fc04aa523
guolei <gl308@qq.com>
2024-03-26 21:55:08 +0800

    fix(tls): fix tls config

--- server/pkg/sfx/cmux_server.go
+++ server/pkg/sfx/cmux_server.go
@@ -26,7 +26,7 @@ func (cmr *ConnectionMuxResult) Execute(
 	g SettingsParams,
 	s SecuritySettingsParams,
 ) (err error) {
-	if s.TLSEnable {
+	if s.TLSServerEnable {
 		cmr.ConnectionMux, err = cmux.NewTlsConnectionMux(l, g.Port, s.ServerCert, s.ServerKey, s.ClientCaCert)
 	} else {
 		cmr.ConnectionMux, err = cmux.NewConnectionMux(l, g.Port)
