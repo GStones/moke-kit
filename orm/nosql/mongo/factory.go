e4e0771eabbcb9cba93e7cc85746e8899077ff8e
GStones <gl308123456@gmail.com>
2024-03-27 17:30:45 +0800

    feat(Database): add database scheme (#132)
    * feat(Database): add database scheme

--- orm/nosql/mongo/factory.go
+++ orm/nosql/mongo/factory.go
@@ -21,23 +21,13 @@ func NewProvider(
 }
 
 func NewMongoClient(
-	connect string,
-	username string,
-	password string,
+	opts *options.ClientOptions,
 ) (*mongo.Client, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
-	credential := options.Credential{
-		Username: username,
-		Password: password,
-	}
-	cOptions := options.Client().ApplyURI(connect)
-	if username != "" && password != "" {
-		cOptions.SetAuth(credential)
-	}
 	if client, err := mongo.Connect(
 		ctx,
-		cOptions,
+		opts,
 	); err != nil {
 		return nil, err
 	} else {
