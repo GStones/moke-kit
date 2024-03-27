ac47bbc0282b746a39dcd1a5e7c798246332a910
guolei <gl308@qq.com>
2024-03-27 16:35:24 +0800

    feat(Database): add database scheme

--- orm/pkg/ofx/mongo_module.go
+++ orm/pkg/ofx/mongo_module.go
@@ -2,14 +2,12 @@ package ofx
 
 import (
 	"context"
-	"fmt"
-	"net/url"
 
 	mongo2 "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 
-	"github.com/gstones/moke-kit/orm/nerrors"
 	"github.com/gstones/moke-kit/orm/nosql/mongo"
 )
 
@@ -33,29 +31,21 @@ func (mr *MongoResult) NewDocument(
 	if n.DatabaseURL == "" {
 		return nil
 	}
-	if u, e := url.Parse(n.DatabaseURL); e != nil {
-		err = e
-	} else if u.Scheme == "mongodb" || u.Scheme == "mongodb+srv" {
-		username := u.User.Username()
-		if n.DatabaseUser != "" {
-			username = n.DatabaseUser
-		}
-		password, _ := u.User.Password()
-		if n.DatabasePassword != "" {
-			password = n.DatabasePassword
-		}
-		conn := fmt.Sprintf("mongodb://%s", u.Host)
-		l.Info("Connect to mongodb", zap.String("url", conn))
-		mr.MongoClient, err = mongo.NewMongoClient(conn, username, password)
-		lc.Append(fx.Hook{
-			OnStop: func(ctx context.Context) error {
-				return mr.MongoClient.Disconnect(ctx)
-			},
-		})
-	} else {
-		l.Error("Invalid mongodb url", zap.String("url", n.DatabaseURL))
-		return nerrors.ErrInvalidNosqlURL
+	cOptions := options.Client().ApplyURI(n.DatabaseURL)
+	if n.DatabaseUser != "" {
+		cOptions.Auth.Username = n.DatabaseUser
 	}
+	if n.DatabasePassword != "" {
+		cOptions.Auth.Password = n.DatabaseUser
+	}
+	l.Info("Connect to mongodb", zap.String("url", n.DatabaseURL))
+	mr.MongoClient, err = mongo.NewMongoClient(cOptions)
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			return mr.MongoClient.Disconnect(ctx)
+		},
+	})
+
 	return
 }
 
